f89c6d113af16b15bfa2e1f2b71ea6e0237844ea
Daniel Patrick Johnson <teknotus@gmail.com>
2015-01-20 00:38:25 -0800

    First release version 0.0.1 Hiding mistakes with rebase

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,9 +3,40 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    mainLayout = new QVBoxLayout();
+    vid = new VideoWidget();
+    camera = new CameraDataFeed();
+    devicePickLayout = new QHBoxLayout();
+    deviceLabel = new QLabel("Device");
+    devicePathEdit = new QLineEdit();
+    deviceLabel->setBuddy(devicePathEdit);
+    devicePickLayout->addWidget(deviceLabel);
+    devicePickLayout->addWidget(devicePathEdit);
+    buttonLayout = new QHBoxLayout();
+    startButton = new QPushButton("start");
+    stopButton = new QPushButton("stop");
+    buttonLayout->addWidget(startButton);
+    buttonLayout->addWidget(stopButton);
+    mainLayout->addWidget(vid);
+    mainLayout->addLayout((devicePickLayout));
+    mainLayout->addLayout(buttonLayout);
+    centerWidget = new QWidget();
+    centerWidget->setLayout(mainLayout);
+    setCentralWidget(centerWidget);
+
+    connect(camera, SIGNAL(newData(void*)), vid, SLOT(updateImage(void*)));
+    connect(startButton, SIGNAL(clicked()), camera, SLOT(startVideo()));
+    connect(stopButton,SIGNAL(clicked()), camera, SLOT(stopVideo()));
+    connect(devicePathEdit,SIGNAL(textChanged(QString)),camera,SLOT(setCameraDevice(QString)));
+
+    settings = new QSettings("solsticlipse", "depthview");
+    if(settings->contains("depthCameraDevice")){
+        devicePathEdit->setText(settings->value("depthCameraDevice").toString());
+    }
 }
 
 MainWindow::~MainWindow()
 {
-
+    settings->setValue("depthCameraDevice",devicePathEdit->text());
+    delete settings;
 }
--- videowidget.cpp
+++ videowidget.cpp
@@ -3,4 +3,30 @@
 VideoWidget::VideoWidget(QWidget *parent) :
     QWidget(parent)
 {
+    setFixedSize(640, 480); // Only handle dimensions of Depth Camera.
+}
+
+void VideoWidget::paintEvent(QPaintEvent *)
+{
+// Only copying the part that changed worsens performance
+
+//    QRect paintRect = event->rect();
+    QPainter painter;
+    painter.begin(this);
+//    painter.drawImage(paintRect.topLeft(),image.copy(paintRect));
+    painter.drawImage(QPoint(0,0),image);
+    painter.end();
+}
+void VideoWidget::updateImage(void * voidData){
+    uchar * data = (uchar *)voidData;
+    image = QImage( 640, 480, QImage::Format_ARGB32 );
+    for(int i = 0 ; i < 640 ; i++){
+        for(int j = 0 ; j < 480 ; j++){
+            uchar big = data[j*640*2 + i*2 + 1];
+            uchar small = data[j*640*2 + i*2];
+            QRgb pix = qRgb( small,255 - (big == 0 ? 255 : big), 0);
+            image.setPixel(i,j,pix);
+        }
+    }
+    repaint();
 }
