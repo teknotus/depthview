b6c578c95af4595c1891d12c6628a3191f8d4868
Daniel Patrick Johnson <teknotus@gmail.com>
2015-01-20 00:34:47 -0800

    First

---
+++ videowidget.h
@@ -0,0 +1,18 @@
+#ifndef VIDEOWIDGET_H
+#define VIDEOWIDGET_H
+
+#include <QWidget>
+
+class VideoWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit VideoWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // VIDEOWIDGET_H
